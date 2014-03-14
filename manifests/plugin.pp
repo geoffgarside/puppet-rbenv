e6fbe57f5b8d6eb1ed540ebe66c47caa8333bf7c
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-14 08:46:40 +0000

    Add an rbenv::plugin resource

---
+++ manifests/plugin.pp
@@ -0,0 +1,24 @@
+define rbenv::plugin (
+  $source,
+  $ensure       = 'present',
+  $revision     = 'master',
+  $plugin_name  = $title,
+) {
+
+  include rbenv
+
+  $rbenv_root   = $::rbenv::rbenv_root
+  $plugins_path = "${rbenv_root}/plugins"
+  $install_path = "${plugins_path}/${plugin_name}"
+
+  if $source !~ /^(git|https):/ {
+    fail("Only git plugins are supported, unable to install from ${source}")
+  }
+
+  vcsrepo { $install_path:
+    ensure    => $ensure,
+    provider  => git,
+    source    => $source,
+    revision  => $revision,
+  }
+}
