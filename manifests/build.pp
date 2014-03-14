cc3a5dfa38eaf8e4c52a9f631ca225d9f17f09ca
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-14 09:19:19 +0000

    Add rbenv::build resource to (un)install rubies

---
+++ manifests/build.pp
@@ -0,0 +1,27 @@
+define rbenv::build (
+  $ensure = 'present',
+  $version = $title,
+) {
+
+  include rbenv
+  include stdlib
+
+  $versions_path = "${::rbenv::rbenv_root}/versions"
+  $install_path  = "${versions_path}/${version}"
+  $default_PATH  = ['/bin', '/usr/bin', '/usr/local/bin']
+
+  Exec {
+    path        => concat($::rbenv::rbenv_PATH, $default_PATH),
+    environment => [$::rbenv::rbenv_ENV],
+  }
+
+  if $ensure == 'absent' {
+    exec { "rbenv uninstall ${version}":
+      onlyif => "test -d ${install_path}",
+    }
+  } else {
+    exec { "rbenv install ${version}":
+      creates => $install_path,
+    }
+  }
+}
