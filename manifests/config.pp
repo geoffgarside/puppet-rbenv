9faf6bc68eddbd567bd13c1a7c72b9156c2162d2
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-14 10:58:55 +0000

    Fix rbenv::config $file_ensure case to selector

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,10 +2,10 @@ class rbenv::config {
 
   $_real_rbenv_plugins = merge($::rbenv::params::rbenv_plugins, $::rbenv::rbenv_plugins)
   if has_key($_real_rbenv_plugins, 'rbenv-default-gems') {
-    $gem_list = join($::rbenv::default_gems, "\n")
-    $file_ensure = case $::rbenv::ensure {
-      'absent': { 'absent' }
-      default:  { 'file' }
+    $gem_list    = join($::rbenv::default_gems, "\n")
+    $file_ensure = $::rbenv::ensure ? {
+      'absent' => 'absent',
+      default  => 'file'
     }
 
     file { "${::rbenv::rbenv_root}/default-gems":
