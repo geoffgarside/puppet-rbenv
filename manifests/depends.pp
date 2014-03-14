d15f537c2891f15e284cd4a17cb6f22dfb988834
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-14 10:02:08 +0000

    Wrap rbenv::depends for readability

--- manifests/depends.pp
+++ manifests/depends.pp
@@ -1,14 +1,33 @@
 class rbenv::depends {
   include stdlib
 
-  ensure_rbenv_dependency(['autoconf', 'automake', 'openssl', 'curl', 'git'])
+  ensure_rbenv_dependency([
+    'autoconf',
+    'automake',
+    'openssl',
+    'curl',
+    'git'
+  ])
 
   case $::osfamily {
     FreeBSD: {
-      ensure_rbenv_dependency(['bash', 'gmake', 'libffi', 'libexecinfo'])
+      ensure_rbenv_dependency([
+        'bash',
+        'gmake',
+        'libffi',
+        'libexecinfo'
+      ])
     }
     Debian: {
-      ensure_rbenv_dependency(['build-essential', 'libtool', 'bison', 'libssl-dev', 'libreadline6', 'libreadline6-dev', 'libc6-dev'])
+      ensure_rbenv_dependency([
+        'build-essential',
+        'libtool',
+        'bison',
+        'libssl-dev',
+        'libreadline6',
+        'libreadline6-dev',
+        'libc6-dev'
+      ])
     }
     default: {
       warn("rbenv::depends not defined for ${::osfamily}, building of rubies may fail")
