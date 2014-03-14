056371a5a06f0b2db43e80a2dcd3fcdd5636283b
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-14 08:53:37 +0000

    Use rbenv::plugin to install ruby-build

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,18 +1,15 @@
 class rbenv::install {
 
   $rbenv_root         = $::rbenv::rbenv_root
-  $ruby_build_root    = "${rbenv_root}/plugins/ruby-build"
-
-  Vcsrepo {
-    ensure    => $::rbenv::ensure,
-    provider  => git,
-  }
 
   vcsrepo { $rbenv_root:
+    ensure   => $::rbenv::ensure,
     revision => $::rbenv::rbenv_version,
     source   => $::rbenv::params::rbenv_source,
+    provider => git,
   }->
-  vcsrepo { $ruby_build_root:
+  rbenv::plugin { 'ruby-build':
+    ensure   => $::rbenv::ensure,
     revision => $::rbenv::ruby_build_version,
     source   => $::rbenv::params::ruby_build_source,
   }
