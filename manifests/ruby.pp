7da6f895ae7ac5c3221db1ae432c0142e2e8f398
Geoff Garside <geoff@geoffgarside.co.uk>
2014-03-17 13:48:04 +0000

    Update rbenv::ruby resource.
    Create an rbenv directory in the users homedir and and symlink their
    versions/$version to shared versions directory.

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,14 +1,32 @@
 define rbenv::ruby (
   $user,
-  $ensure  = 'present',
-  $version = $title,
+  $ensure   = 'present',
+  $version  = $title,
 ) {
 
   include rbenv
 
   if ! defined(Rbenv::Build[$version]) {
     rbenv::build { $version:
-      ensure => 'present',
+      ensure => $ensure,
     }
+  }
+
+  $rbenv_root             = "/home/${user}/.rbenv"
+  $rbenv_versions         = "${rbenv_root}/versions"
+  $rbenv_shared_versions  = "${::rbenv::rbenv_root}/versions"
+  
+  $link_ensure = $ensure ? {
+    'absent' => 'absent',
+    default  => 'link'
+  }
+
+  rbenv::install { $rbenv_root:
+    user => $user,
+  }->
+  rbenv::config { $user: }->
+  file { "${rbenv_versions}/${version}":
+    ensure => $link_ensure,
+    target => "${rbenv_shared_versions}/${version}",
   }
 }
