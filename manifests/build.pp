7e224d189e3ad4b6c40f8656b8c2310827588c66
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-01 23:00:58 +0000

    Fix exec HOME specification

--- manifests/build.pp
+++ manifests/build.pp
@@ -5,27 +5,24 @@ define rbenv::build (
   include stdlib
   include rbenv
 
-  $homedir = $::rbenv::user ? {
-    'root'  => $::rbenv::params::root_homedir,
-    default => "${::rbenv::params::user_homedir}/${::rbenv::user}",
-  }
-
   $versions_path = "${::rbenv::rbenv_root}/versions"
   $install_path  = "${versions_path}/${version}"
   $default_PATH  = ['/bin', '/usr/bin', '/usr/local/bin']
   $rbenv_PATH    = any2array($::rbenv::rbenv_PATH)
   $rbenv_ENV     = any2array($::rbenv::rbenv_ENV)
   $exec_PATH     = concat($rbenv_PATH, $default_PATH)
-  $exec_ENV      = concat($rbenv_ENV, ["HOME=${homedir}"])
+  $exec_ENV      = concat($rbenv_ENV, [])
 
   if $ensure == 'absent' {
     exec { "rbenv uninstall ${version}":
       onlyif      => "test -d ${install_path}",
+      user        => $::rbenv::user,
       path        => $exec_PATH,
       environment => $exec_ENV,
     }
   } else {
     exec { "rbenv install ${version}":
+      user        => $::rbenv::user,
       path        => $exec_PATH,
       environment => $exec_ENV,
       logoutput   => 'on_failure',
