eb935809204266fa0e34bec8b168c9ad4404a941
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-01 20:41:11 +0000

    Remove compile_ENV from rbenv::build

--- manifests/build.pp
+++ manifests/build.pp
@@ -11,7 +11,7 @@ define rbenv::build (
   $rbenv_PATH    = any2array($::rbenv::rbenv_PATH)
   $rbenv_ENV     = any2array($::rbenv::rbenv_ENV)
   $exec_PATH     = concat($rbenv_PATH, $default_PATH)
-  $exec_ENV      = concat($rbenv_ENV, $compile_ENV)
+  $exec_ENV      = $rbenv_ENV
 
   if $ensure == 'absent' {
     exec { "rbenv uninstall ${version}":
