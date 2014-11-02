048f2a2703fb0c79e06412ae9dab9b194ceaf5d7
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-02 21:24:06 +0000

    Add user support

--- manifests/ruby.pp
+++ manifests/ruby.pp
@@ -1,12 +1,74 @@
 define rbenv::ruby (
   $ensure  = 'present',
   $version = $name,
+  $user    = undef,
+  $home    = undef,
+  $group   = undef,
+  $global  = false,
 ) {
   include rbenv
 
   if ! defined(Rbenv::Build[$version]) {
     rbenv::build { $version:
       ensure => $ensure,
+    }
+  }
+
+  if $user && $user != 'root' {
+    $_homedir = $user ? {
+      'root'  => $::rbenv::params::root_homedir,
+      default => "${::rbenv::params::user_homedir}/${user}"
+    }
+
+    $_group = $user ? {
+      'root'  => '0',
+      default => $user,
+    }
+
+    $user_group      = pick($group, $_group)
+    $user_homedir    = pick($home, $_homedir)
+    $user_rbenv_root = "${user_homedir}/.rbenv"
+
+    if ! defined(Rbenv::User[$user]) {
+      rbenv::user { $user:
+        ensure     => $ensure,
+        group      => $user_group,
+        home       => $user_homedir,
+        rbenv_root => $user_rbenv_root,
+        require    => Rbenv::Build[$version],
+      }
+    }
+
+    $link_ensure = $ensure ? {
+      'absent' => 'absent',
+      default  => 'link',
+    }
+
+    file { "${user_rbenv_root}/versions/${version}":
+      ensure => $link_ensure,
+      target => "${::rbenv::rbenv_root}/versions/${version}",
+      owner  => $user,
+      group  => $user_group,
+      require => [
+        Rbenv::Build[$version],
+        Rbenv::User[$user],
+      ]
+    }
+
+    if $global {
+      $file_ensure = $ensure ? {
+        'absent' => 'absent',
+        default  => 'file',
+      }
+
+      file { "${user_rbenv_root}/version":
+        ensure  => $file_ensure,
+        content => "${version}\n",
+        owner   => $user,
+        group   => $user_group,
+        mode    => '0644',
+        require => File["${user_rbenv_root}/versions/${version}"],
+      }
     }
   }
 }
---
+++ manifests/user.pp
@@ -0,0 +1,120 @@
+define rbenv::user(
+  $ensure     = 'present',
+  $user       = $name,
+  $home       = undef,
+  $group      = undef,
+  $rbenv_root = undef,
+) {
+
+  $_homedir = $user ? {
+    'root'  => $::rbenv::params::root_homedir,
+    default => "${::rbenv::params::user_homedir}/${user}"
+  }
+
+  $_group = $user ? {
+    'root'  => '0',
+    default => $user,
+  }
+
+  $user_group      = pick($group, $_group)
+  $user_homedir    = pick($home, $_homedir)
+  $user_rbenv_root = pick($rbenv_root, "${user_homedir}/.rbenv")
+
+  $dir_ensure = $ensure ? {
+    'absent' => 'absent',
+    default  => 'directory',
+  }
+
+  # Create the users rbenv root directory
+  file { $user_rbenv_root:
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+  }
+
+  # Create following dirs
+  #  * .rbenv/versions
+  file { "${user_rbenv_root}/versions":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    purge   => false,
+    replace => false,
+    require => File[$user_rbenv_root],
+  }
+
+  # Create following dirs, keep in sync
+  #  * .rbenv/bin
+  #  * .rbenv/completions
+  #  * .rbenv/libexec
+  #  * .rbenv/plugins
+  #  * .rbenv/rbenv.d
+  #  * .rbenv/shims
+  #  * .rbenv/src
+  file { "${user_rbenv_root}/bin":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    source  => "${::rbenv::rbenv_root}/bin",
+    recurse => true,
+    force   => true,
+    purge   => true,
+    replace => true,
+    require => File[$user_rbenv_root],
+  }
+
+  file { "${user_rbenv_root}/completions":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    source  => "${::rbenv::rbenv_root}/completions",
+    recurse => true,
+    force   => true,
+    purge   => true,
+    replace => true,
+    require => File[$user_rbenv_root],
+  }
+
+  file { "${user_rbenv_root}/libexec":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    source  => "${::rbenv::rbenv_root}/libexec",
+    recurse => true,
+    force   => true,
+    purge   => true,
+    replace => true,
+    require => File[$user_rbenv_root],
+  }
+
+  file { "${user_rbenv_root}/rbenv.d":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    source  => "${::rbenv::rbenv_root}/rbenv.d",
+    recurse => true,
+    force   => true,
+    purge   => true,
+    replace => true,
+    require => File[$user_rbenv_root],
+  }
+
+  file { "${user_rbenv_root}/src":
+    ensure  => $dir_ensure,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0755',
+    source  => "${::rbenv::rbenv_root}/src",
+    recurse => true,
+    force   => true,
+    purge   => true,
+    replace => true,
+    require => File[$user_rbenv_root],
+  }
+}
