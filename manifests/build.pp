6f1826f7c00217cbbf278cb185fcbf09de2e3b98
Geoff Garside <geoff@geoffgarside.co.uk>
2014-11-02 10:40:35 +0000

    Fix missing { on if condition

--- manifests/build.pp
+++ manifests/build.pp
@@ -24,7 +24,7 @@ define rbenv::build (
 
     # Mac OS X requires GCC for Ruby versions 1.9.3-p0 and below
     # NOTE: This requires that the homebrew/dupes repo has been tapped
-    if $::osfamily == 'Darwin'
+    if $::osfamily == 'Darwin' {
       if versioncmp($version, "1.9.3-p0") < 1 or $version =~ /^ree/ {
         if ! defined(Package['apple-gcc42']) {
           package { 'apple-gcc42':
